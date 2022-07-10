df70e96d478b1d2ad4a690f5cce5739f5c8e0b69
krisnasw <krisnasw@live.com>
2022-07-10 19:04:50 +0700

    Add example files

---
+++ src/auth/application/injection.token.ts
@@ -0,0 +1,6 @@
+export enum InjectionToken {
+  ACCOUNT_REPOSITORY = 'AuthRepository',
+  ACCOUNT_QUERY = 'AuthQuery',
+  INTEGRATION_EVENT_PUBLISHER = 'IntegrationEventPublisher',
+  EVENT_STORE = 'EventStore',
+}
